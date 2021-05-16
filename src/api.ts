5a72c6435d8c2dda4472a3f6f912732e515d4fda
Joonatan Peltonen <joonatan.peltonen@gmail.com>
2021-05-16 23:29:20 +0300

    Update getData function

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,10 @@
 import { City, Weather } from "./types";
 
-const API_KEY = "bed7115776ca1cb02795cb60f66b4ef8";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const getCityData = async (city: string): Promise<City[]> => {
   return fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`
   ).then((res) => {
     if (res.ok) {
       return res.json();
@@ -13,9 +13,8 @@ export const getCityData = async (city: string): Promise<City[]> => {
 };
 
 export const getCityWeather = async (city: City): Promise<Weather> => {
-  const cityData = await getCityData(city.name);
-  const lat = cityData[0].lat;
-  const lon = cityData[0].lon;
+  const lat = city.lat;
+  const lon = city.lon;
 
   return fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=alerts,minutely,current&appid=${API_KEY}`
@@ -28,10 +27,14 @@ export const getCityWeather = async (city: City): Promise<Weather> => {
 
 export const getData = async (city: string) => {
   const cityData = await getCityData(city);
-  if (cityData === undefined) {
-    alert("Weather data not found.");
+  if (cityData === undefined || cityData.length === 0) {
+    alert("City data not found.");
     return;
   }
   const weatherData = await getCityWeather(cityData[0]);
+  if (weatherData === undefined) {
+    alert("Weather data not found.");
+    return;
+  }
   return { cityData, weatherData };
 };
